0b2fe3bb9e4ee95adfb7c4490365a57ea5e0afa7
VinayakGalande01 <165789463+VinayakGalande01@users.noreply.github.com>
2025-03-07 14:50:07 +0530

    Update apps.py

--- apps.py
+++ apps.py
@@ -1,6 +1,15 @@
+# Import the AppConfig class from Django's application module
 from django.apps import AppConfig
 
-
+# Define a configuration class for the Django app
 class ApiConfig(AppConfig):
+    """
+    Configuration class for the 'api' Django application.
+    This class is used to configure settings specific to the app.
+    """
+    
+    # Specifies the default field type for auto-generated primary keys
     default_auto_field = 'django.db.models.BigAutoField'
+
+    # Name of the Django app
     name = 'api'
