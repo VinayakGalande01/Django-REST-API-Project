d8e1e1a616bc5eec0a9f8994169c78ef3ace89f2
VinayakGalande01 <165789463+VinayakGalande01@users.noreply.github.com>
2025-04-16 07:31:40 +0530

    api views.py

--- views.py
+++ views.py
@@ -1,20 +1,37 @@
-# Import necessary Django response modules
-from django.http import HttpResponse,JsonResponse
+from rest_framework import viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from api.models import Company, Employee
+from api.serializers import CompanySerializer, EmployeeSerializer
+from django.shortcuts import render
 
-# Define a view function to handle the home page request
-def home_page(request):
-"""
-Handles requests to the home page and returns a JSON response.
-"""
-    # Log a message to indicate the home page was requested
-    print("home page requested")
 
-    # List of friends to be returned as a JSON response
-    friends = [
-        'ankit',
-        'ravi',
-        "uttam"
-    ]
+# Company ViewSet - Provides CRUD operations for the Company model
+class CompanyViewSet(viewsets.ModelViewSet):
+    queryset = Company.objects.all()  # Queryset defines the data that will be used by the viewset
+    
+    serializer_class = CompanySerializer  # Specify the serializer class to use for the Company model
 
-    # Return the list as a JSON response with safe=False (since it's a list, not a dictionary)
-    return JsonResponse(friends,safe=False)
+    # Custom endpoint to retrieve all employees for a specific company
+    # URL pattern: /companies/{companyID}/employees/
+    @action(detail=True, methods=['get'])
+    def employees(self, request, pk=None):#print('get employees of ',pk,'company') cross check whether its working or not
+        try:
+             # Get the Company object by its primary key (pk), or return 404 if not found
+            company = Company.objects.get(pk=pk)
+            emps = Employee.objects.filter(company=company)
+            emps_serializer = EmployeeSerializer(emps, many=True, context={'request': request})
+            return Response(emps_serializer.data)
+        except Exception as e:
+            print(e)
+            # In case of an error, return a 400 response with the error message
+            return Response({
+                'message': 'Company might not exists !!'
+            })
+
+# Employee ViewSet - Provides CRUD operations for the Employee model
+class EmployeeViewSet(viewsets.ModelViewSet):
+    # Queryset defines the data that will be used by the viewset
+    queryset = Employee.objects.all()
+    # Specify the serializer class to use for the Employee model
+    serializer_class = EmployeeSerializer
